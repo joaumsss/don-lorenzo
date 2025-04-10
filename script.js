cf178c4b3d92b769b90cbd6c4d2eb458c5019f5e
joaumsss <joaumsss@gmail.com>
2025-04-10 16:17:46 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function enviarPedido() {
+    const selecionados = document.querySelectorAll('input[type=checkbox]:checked');
+    if (selecionados.length === 0) {
+        alert('Selecione pelo menos 1 item para fazer o pedido.');
+        return;
+    }
+
+    let mensagem = "Olá, gostaria de fazer o seguinte pedido:%0A";
+    selecionados.forEach(item => {
+        mensagem += "- " + item.value + "%0A";
+    });
+
+    const telefone = "5551996179364"; // Número WhatsApp
+    const url = `https://wa.me/${telefone}?text=${mensagem}`;
+
+    window.open(url, '_blank');
+}
